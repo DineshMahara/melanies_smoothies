b729ca668e3ee2ea7d572094d7fa2bdda13c326f
DineshMahara <78276055+DineshMahara@users.noreply.github.com>
2024-09-11 22:20:32 +0100

    SEARCH_ON

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ cnx = st.connection("snowflake")
 session = cnx.session()
 #session = get_active_session()
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('Fruit_name'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 
 ingredients_string = ('')
