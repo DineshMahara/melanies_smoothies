89e4d1608f9bef983184f86e1c37ad1d8ab87a21
DineshMahara <78276055+DineshMahara@users.noreply.github.com>
2024-09-11 21:42:39 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 # Import python packages
-import streamlit as st
-#from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col
 import requests
+import streamlit as st
+from snowflake.snowpark.functions import col
+
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response)
