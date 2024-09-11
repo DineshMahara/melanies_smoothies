ae9f4ce8576287e2dbf227457680195cf7b99b02
DineshMahara <78276055+DineshMahara@users.noreply.github.com>
2024-09-11 21:57:01 +0100

    adda

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,7 +39,7 @@ if ingredients_list:
     session.sql(my_insert_stmt).collect()
 
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-    fv_df=st.dataframe(data=fruityvice_response.json() , use_container_width=True )
+    fv_df = st.dataframe(data=fruityvice_response.json() , use_container_width=True )
 
     st.success('Your Smoothie is ordered , '+ name_on_order +' !' ,icon="✅")
     
